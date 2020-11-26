00a002d205824344e7a1751495ae61a82d0c9048
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2020-11-26 16:05:41 +0800

    create: added activities section

--- js/main.js
+++ js/main.js
@@ -28,3 +28,27 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: false,
   },
 })
+
+$(window).scroll(function(){
+  $('.newsletter').bgscroll({
+    direction: 'bottom', // направление bottom или top
+    bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+    debug: false, // Режим отладки
+    min:0, // минимальное положение (в %) на которое может смещаться фон
+    max:100 // максимальное положение (в %) на которое может смещаться фон
+  });
+})
+
+/*
+$(document).ready(function(){
+  $(document).scroll(function(){
+    $('.newsletter').bgscroll({
+      direction: 'bottom', // направление bottom или top
+      bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+      debug: false, // Режим отладки
+      min:0, // минимальное положение (в %) на которое может смещаться фон
+      max:100 // максимальное положение (в %) на которое может смещаться фон
+    });
+  });
+});
+*/
