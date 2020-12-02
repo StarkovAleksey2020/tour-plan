8571e598a46a4172fe97ad895886f171ee2a397a
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2020-12-02 17:01:45 +0800

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -39,16 +39,7 @@ $(window).scroll(function(){
   });
 })
 
-/*
-$(document).ready(function(){
-  $(document).scroll(function(){
-    $('.newsletter').bgscroll({
-      direction: 'bottom', // направление bottom или top
-      bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
-      debug: false, // Режим отладки
-      min:0, // минимальное положение (в %) на которое может смещаться фон
-      max:100 // максимальное положение (в %) на которое может смещаться фон
-    });
-  });
-});
-*/
+var menuButton = document.querySelector(".menu-button")
+menuButton.addEventListener('click', function () {
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+})
