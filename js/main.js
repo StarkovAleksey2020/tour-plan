355cab21924d59a1ca0caea49c976694c31a1a36
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2020-12-04 16:21:04 +0800

    create: added modal form

--- js/main.js
+++ js/main.js
@@ -1,45 +1,73 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+    var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: "cube",
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-})
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: "cube",
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  })
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-})
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  })
 
-$(window).scroll(function(){
-  $('.newsletter').bgscroll({
-    direction: 'bottom', // направление bottom или top
-    bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
-    debug: false, // Режим отладки
-    min:0, // минимальное положение (в %) на которое может смещаться фон
-    max:100 // максимальное положение (в %) на которое может смещаться фон
+  $(window).scroll(function(){
+    $('.newsletter').bgscroll({
+      direction: 'bottom', // направление bottom или top
+      bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+      debug: false, // Режим отладки
+      min:0, // минимальное положение (в %) на которое может смещаться фон
+      max:100 // максимальное положение (в %) на которое может смещаться фон
+    });
+  })
+
+  var menuButton = $(".menu-button")
+  menuButton.on('click', function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  })
+  
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal)
+
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      closeModal(e);
+    }
   });
-})
 
-var menuButton = document.querySelector(".menu-button")
-menuButton.addEventListener('click', function () {
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
-})
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+
+  }
+});
