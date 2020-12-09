7d11866741197444dcb26b00e4a5c883e8d26bc5
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2020-12-09 09:49:06 +0800

    added: message form invalid layout

--- js/main.js
+++ js/main.js
@@ -63,6 +63,8 @@ $(document).ready(function () {
     modalDialog.addClass("modal__dialog--visible");
     $('input').val('');
     $('textarea').val('');
+    $("input").removeClass("invalid");
+    $("label").remove(".invalid");
     modalDialog.scrollTop(0);
   }
   function closeModal(event) {
@@ -71,7 +73,9 @@ $(document).ready(function () {
     var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
-  }
+    $("input").removeClass("invalid");
+    $("label").remove(".invalid");
+}
 
   // Обработка форм
   $('.form').each(function () {
@@ -89,11 +93,11 @@ $(document).ready(function () {
           minlength: "Minimum field length 2 characters",
         },
         phone: {
-          required: "Required field",
+          required: "Please specify your phone number",
         },
         email: {
-          required: "Required field",
-          email: "Your email address must be in the format name@domain.com",
+          required: "Please specify your email",
+          email: "Email address format is name@domain.com",
         },
       },
     });
