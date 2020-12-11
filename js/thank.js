1979f54f68bd6d5bcd5a2ccc45fe0f46380e9c77
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2020-12-11 22:27:43 +0800

    added: merci page sripts created

---
+++ js/thank.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+  var menuButton = $(".menu-button")
+  menuButton.on('click', function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  })
+  
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal)
+
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      closeModal(e);
+    }
+  });
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+    $('input').val('');
+    $('textarea').val('');
+    $("input").removeClass("invalid");
+    $("label").remove(".invalid");
+    modalDialog.scrollTop(0);
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+    $("input").removeClass("invalid");
+    $("label").remove(".invalid");
+}
+
+  jQuery.validator.addMethod("emailfull", function(value, element) {
+ return this.optional(element) || /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(value);
+  }, "Please enter valid email address!");
+  
+  // Обработка форм
+  $('.form').each(function () {
+    $(this).validate({
+      rules: {
+        email: {
+          required: true,
+          email: true,
+          emailfull: true
+        }
+      },
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Minimum field length 2 characters",
+        },
+        phone: {
+          required: "Please specify your phone number",
+        },
+        email: {
+          required: "Please specify your email",
+          email: "Email address format is name@domain.com",
+        },
+      },
+    });
+  });
+
+  // Маска номера телефона
+  $(".phoneInput").mask("+7 (999) 999-99-99");
+
+});
